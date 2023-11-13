bae61e69707c4327f63ab2b98ef8790c8854db9b
Ultra-tech-code <hizick27@gmail.com>
2023-11-13 10:44:44 +0100

    env installation and hardhat config settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+
+require('dotenv').config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    telos_testnet: {
+      url: "https://testnet.telos.net/evm",
+      accounts: [process.env.TELOS_TESTNET_PRIVATE_KEY],
+      chainId: 41,
+    },
+  },
 };
 
 export default config;
